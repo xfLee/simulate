9db636ee53e9bca7d5e4130162a1dbcef1a39c07
xfLee <xfLee@users.noreply.github.com>
2017-11-17 22:53:45 +0800

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -1,17 +1,25 @@
 import numpy as np
 
 iter = 1e+07
-i = 0
+i = 1
 while i <= iter:
-    beta1 = np.random.normal(loc = 1.4, scale = 1.2, size = 1)
+    # Set beta2, beta3, beta4 to be 0
+    sample1 = np.random.normal(loc = 1.4, scale = 1.2, size = 1)
     sigma = 1.2
-    qz1 = 1 / (np.sqrt(2 * np.pi) * sigma**2) * np.exp(-0.5 * (beta1 - 1.4)**2 / sigma**2)
+    # qz is reference distribution, like Gaussian(1.4, 1.2).
+    # Change default parameters to your convenience
+    qz1 = 1 / (np.sqrt(2 * np.pi) * sigma**2) * np.exp(-0.5 * (sample1 - 1.4)**2 / sigma**2)
     k = 3
     u1 = np.random.uniform(low = 0, high = k * qz1, size = 1)
-
-    pz1 =  0.3 * np.exp(-(beta1 - 0.3) ** 2) + 0.7 * np.exp(-(beta1 -2) ** 2 / 0.3)
-    sample1 = beta1[pz1 >= u1]
+    
+    # pz is your target distribution to sample. Substitute to your setting distribution
+    pz1 =  0.3 * np.exp(-(sample1 - 0.3) ** 2) + 0.7 * np.exp(-(sample1 -2) ** 2 / 0.3)
+    if pz1 >= u1:
+        beta1 = sample1
+    else: 
+        beta1 = 0
     '''
-    Codes to sample beta2, beta3, beta4 go here. Substitute parameters to corresponding settings.
+    Codes to sample beta2, beta3, beta4 go here, similar to sample beta1. 
+    Substitute parameters to corresponding settings following the explanation.
     '''
     i += 1
